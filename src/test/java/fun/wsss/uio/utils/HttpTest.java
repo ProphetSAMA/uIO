6ff4934fdadd1ef5a3a1c202f3e8b9b0fbd5a794
Wsssfun <3085971975@qq.com>
2024-09-03 23:26:32 +0800

    Update HttpTest.java

--- src/test/java/fun/wsss/uio/utils/HttpTest.java
+++ src/test/java/fun/wsss/uio/utils/HttpTest.java
@@ -1,16 +1,5 @@
 package fun.wsss.uio.utils;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class HttpTest {
 
         @org.junit.jupiter.api.Test
